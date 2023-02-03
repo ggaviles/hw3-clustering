3047dd0aad9a83580a756fd37439eb159182f3e0
Giovanni Aviles <giovanni.aviles@ucsf.edu>
2023-02-03 03:19:22 -0800

    Updated silhouette.py to return the simplified silhouette score for each observation

--- cluster/silhouette.py
+++ cluster/silhouette.py
@@ -11,7 +11,7 @@ class Silhouette:
             none
         """
 
-    def score(self, x: np.ndarray, y: np.ndarray) -> np.ndarray:
+    def score(self, mat: np.ndarray, centroids: np.ndarray) -> np.ndarray:
         """
         calculates the silhouette score for each of the observations
 
@@ -28,6 +28,7 @@ class Silhouette:
         """
 
         # Check if X is in the right format
+        """
         try:
             x.ndim == 2
         except TypeError:
@@ -38,13 +39,20 @@ class Silhouette:
             y.ndim == 1
         except TypeError:
             print('Incorrect number of dimensions:' + y.ndim)
+        """
+        distance_matrix = np.array(([([np.linalg.norm(m - c) ** 2 for c in centroids]) for m in mat]))
 
-        KMeans.distance_from_centroids()
+        intra_cluster_dist = np.array([min([np.linalg.norm(m - c) ** 2 for c in centroids]) for m in mat])
 
+        for i in range(len(distance_matrix)):
+            distance_matrix[i] = sorted(distance_matrix[i])
 
+        mid_dist = []
+        for i in range(len(distance_matrix)):
+            mid_dist.append(distance_matrix[i][1])
 
+        min_inter_cluster_dist = mid_dist
 
+        silhouette_matrix = (min_inter_cluster_dist - intra_cluster_dist) / min_inter_cluster_dist
 
-
-
-
+        return silhouette_matrix
